b984094d8aab509a203e7fbba47c7ddf895e2cbc
Zachary Drayer <zacharydrayer@gmail.com>
2023-07-30 18:26:36 -0700

    fix deprecated string api

--- j2s/StringTransformations.swift
+++ j2s/StringTransformations.swift
@@ -6,7 +6,7 @@ internal extension String {
 
         var generatedClassName = camelCased().depluralize()
         let replacementRange = (generatedClassName.startIndex ..< generatedClassName.index(after: generatedClassName.startIndex))
-        let initial = generatedClassName.substring(with: replacementRange)
+        let initial = generatedClassName[replacementRange]
         generatedClassName.replaceSubrange(replacementRange, with: initial.uppercased())
         return generatedClassName
     }
@@ -16,49 +16,49 @@ internal extension String {
 
         let name = self
         if name.hasSuffix("uies") {
-            return name.substring(to: name.index(name.endIndex, offsetBy: -4)).appending("y")
+			return name[..<name.index(name.endIndex, offsetBy: -4)].appending("y")
         }
         if name.hasSuffix("ices") {
             if [ "matrices" ].contains(name) {
                 return "matrix"
             }
 
-            return name.substring(to: name.index(name.endIndex, offsetBy: -4)).appending("ex")
+			return name[..<name.index(name.endIndex, offsetBy: -4)].appending("ex")
         }
         if name.hasSuffix("eaux") || name.hasSuffix("eaus") {
-            return name.substring(to: name.endIndex)
+			return String(name[..<name.endIndex])
         }
         if name.hasSuffix("ves") {
-            return name.substring(to: name.index(name.endIndex, offsetBy: -3)).appending("f")
+			return name[..<name.index(name.endIndex, offsetBy: -3)].appending("f")
         }
         if name.hasSuffix("ata") {
-            return name.substring(to: name.index(name.endIndex, offsetBy: -3)).appending("ma")
+            return name[..<name.index(name.endIndex, offsetBy: -3)].appending("ma")
         }
         if name.hasSuffix("mas") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.index(before: name.endIndex)])
         }
         if name.hasSuffix("ies") {
             if [ "series", "species" ].contains(name) {
                 return name
             }
 
-            return name.substring(to: name.index(name.endIndex, offsetBy: -3)).appending("y")
+            return name[..<name.index(name.endIndex, offsetBy: -3)].appending("y")
         }
         if name.hasSuffix("ses") {
-            return name.substring(to: name.index(name.endIndex, offsetBy: -3)).appending("is")
+            return name[..<name.index(name.endIndex, offsetBy: -3)].appending("is")
         }
         if name.hasSuffix("es") {
             if name == "houses" {
                 return "house"
             }
 
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
         if name.hasSuffix("ae") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
         if name.hasSuffix("um") {
-            return name.substring(to: name.endIndex).appending("a")
+            return name[..<name.endIndex].appending("a")
         }
         if name.hasSuffix("us") {
             if [ "status" ].contains(name) {
@@ -66,26 +66,26 @@ internal extension String {
             }
 
             if [ "corpus" ].contains(name) {
-                return name.substring(to: name.index(before: name.endIndex)).appending("ora")
+                return name[..<name.index(before: name.endIndex)].appending("ora")
             }
 
             if [ "genus" ].contains(name) {
-                return name.substring(to: name.index(before: name.endIndex)).appending("era")
+                return name[..<name.index(before: name.endIndex)].appending("era")
             }
 
-            return name.substring(to: name.index(before: name.endIndex)).appending("i")
+            return name[..<name.index(before: name.endIndex)].appending("i")
         }
         if name.hasSuffix("en") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
         if name.hasSuffix("fs") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
         if name.hasSuffix("ys") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
         if name.hasSuffix("s") {
-            return name.substring(to: name.index(before: name.endIndex))
+            return String(name[..<name.endIndex])
         }
 
         // list of apophonic and irregular plurals
@@ -116,7 +116,7 @@ internal extension String {
             let endOfNextRange = underlineRange.upperBound == camelCased.endIndex ? camelCased.endIndex : camelCased.index(after: underlineRange.upperBound)
             let nextRange = (underlineRange.upperBound ..< endOfNextRange)
             let replacementRange = (underlineRange.lowerBound ..< nextRange.upperBound).clamped(to: (underlineRange.lowerBound ..< camelCased.endIndex))
-            let initial = camelCased.substring(with: nextRange)
+            let initial = camelCased[nextRange]
             camelCased.replaceSubrange(replacementRange, with: initial.uppercased())
         }
 
